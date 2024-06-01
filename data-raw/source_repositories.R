9e0a648c413cee84015b8849574b70912f6f8ffc
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-06-01 13:00:28 +0200

    Add source_repositories csv

---
+++ data-raw/source_repositories.R
@@ -0,0 +1,35 @@
+library(ctv)
+library(purrr)
+library(dplyr)
+
+source_from_desc <- read.ctv("Epidemiology.md") |>
+  pluck("packagelist", "name") |>
+  pkgsearch::cran_packages() |>
+  transmute(
+    package = Package,
+    github_repo = dplyr::coalesce(
+      stringr::str_match(URL, "https://github.com/([^/]+/[^/,]+)")[, 2],
+      stringr::str_match(BugReports, "https://github.com/([^/]+/[^/,]+)/issues")[, 2]
+    )
+  )
+
+source_from_manual <- tribble(
+  ~ package, ~ github_repo,
+  "coarseDataTools", "nickreich/coarseDataTools",
+  "covid19france", "Covid19R/covid19france",
+  "covid19us", "aedobbyn/covid19us",
+  "epiR", "mstevenson888/epiR",
+  "EpiReport", "EU-ECDC/EpiReport",
+  "episensr", "dhaine/episensr",
+  "etm", "mclements/etm",
+  "mediation", "kosukeimai/mediation",
+  "outbreaker2", "reconhub/outbreaker2",
+  "riskCommunicator", "jgrembi/riskCommunicator",
+  "TransPhylo", "xavierdidelot/TransPhylo",
+  "tsiR", "adbecker/tsiR"
+)
+
+anti_join(source_from_desc, source_from_manual, by = "package") |>
+  full_join(source_from_manual) |>
+  dplyr::arrange(package) |>
+  write.csv("data/source_repositories.csv", row.names = FALSE)
